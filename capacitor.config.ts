375a0c241aedc771a674186489090cb1e2d21c65
Sergio Guzman <sealguzmanal@ittepic>
2023-03-27 19:30:18 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.p03',
+  appName: 'p03',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
